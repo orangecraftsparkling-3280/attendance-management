d635543218e89bed42fa76252267ca01b660d487
asari kazuyuki <orange.craft.sparkling@gmail.com>
2026-08-02 20:21:27 +0900

    404・419・500エラー時に、共通デザインのエラーページを表示する
    【問題】
    存在しないページにアクセスした場合(404)、フォームの送信が
    期限切れになった場合(419、今回のセッション中にも実際に
    発生した)、予期しないエラーが起きた場合(500)、いずれも
    Laravel標準の飾り気のない画面がそのまま表示されていた。
    このアプリ独自のヘッダー(ロゴ・ナビゲーション)も表示されず、
    利用者にとって何が起きたのか分かりにくい状態だった。
    
    【対応】
    404・419・500それぞれについて、専用のエラーページを作成した。
    共通レイアウト(layouts.app)を使うことでヘッダーはそのまま
    表示され、中央に「何が起きたか」を短く伝えるメッセージと、
    トップページへ戻るボタンを表示するようにした。
    403エラーは、このアプリの中で実際に発生する経路が無い
    (権限が無い場合はログイン画面へのリダイレクトにしている)
    ため、今回は作成していない。
    
    対象:
    - src/resources/views/errors/404.blade.php(新規)
    - src/resources/views/errors/419.blade.php(新規)
    - src/resources/views/errors/500.blade.php(新規)
    - src/public/css/common.css(エラーページ用のスタイル追加)
    
    既存のテスト(34件)はすべて成功することを確認済み。
    また、実際にサーバーを起動して404・419・500それぞれの
    状況を発生させ、共通ヘッダー付きの新しいエラーページが
    正しいステータスコードで表示されることを確認済み。

---
+++ src/resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="error-page">
+    <h1>500</h1>
+    <p>エラーが発生しました。しばらくしてからもう一度お試しください。</p>
+    <a href="/" class="error-page_link">トップページへ戻る</a>
+</div>
+@endsection
